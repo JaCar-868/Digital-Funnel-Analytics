6d146f12848164c2fa2f24b934f318eb1f8b198d
Joshua Carpenter <habashuba@gmail.com>
2025-05-16 19:08:46 -0400

    Create funnel.py

---
+++ modules/funnel.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+def compute_funnel_metrics(df: pd.DataFrame) -> pd.DataFrame:
+    """
+    Define steps and compute:
+      - conversion rates
+      - time-to-convert distributions
+      - cohort retention curves
+    """
+    # TODO: implement
+    results = pd.DataFrame()
+    return results
